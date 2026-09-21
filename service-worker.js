b7cddba8ecb7aff4c3aa5107cc383a3472590789
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-21 23:53:48 +0300

    chore: refresh PWA cache for dock contract

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'engineering-tools-v1.0.0';
+const CACHE='engineering-tools-v1.0.0-dockfix1';
 const ASSETS = ['./','./index.html','./styles.css','./data.js','./core.js','./app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
